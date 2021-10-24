77c60ae88d3c03b9e13a57bc073955ed6301da0b
christinew2 <christinewei2@gmail.com>
2021-10-23 22:20:30 -0500

    remove navbar images

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,35 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './NavBar.css'
-import nest from '../images/login.png'
-import fowl from '../images/partyfowl-nav.png'
-import goose from '../images/goose.png'
+
+// Assets
 
 
 const NavBar = ({ user, handleLogout }) => {
 	return (
 		<nav className="nav-bar">
-			<div className='nav-parent'>
 			{user ? (
 				<>
-					<NavLink className='Home nav-a' to='/'><img className='goose-icon' src={fowl} alt="Logo" /></NavLink>
-					<span className='header-img' ><img className='goose-icon' src={goose} alt="a goose" /></span>  
+					<NavLink className='Home nav-a' to='/'>Home</NavLink>
 					<div className='user-links'>
-					<NavLink className='nav-a logout' to='/' onClick={handleLogout}>Log out</NavLink>
-					<NavLink className='nav-a profile' to='/profile'>Profile</NavLink>
+						<NavLink className='nav-a logout' to='/' onClick={handleLogout}>Log Out</NavLink>
+						<NavLink className='nav-a profile' to='/profile'>Profile</NavLink>
 					</div>
 				</>
 			) : (
 				<>
-					<NavLink className='Home nav-a' to='/'><img className='goose-icon' src={fowl} alt="Logo" /></NavLink>
-					<span className='header-img' ><img className='goose-icon flying' src={goose} alt="a goose" /></span> 
-					<span className='form-links'>
-					<NavLink className='login nav-a' to="/login"><img className='nest-icon' src={nest} alt="an egg" /><br/></NavLink>
-					<NavLink className='signup nav-a' to="/signup">Sign Up</NavLink>
-					</span>
+					<NavLink className='Home nav-a' to='/'>Home</NavLink>
+					<div className='form-links'>
+						<NavLink className='login nav-a' to="/login">Log In</NavLink>
+						<NavLink className='signup nav-a' to="/signup">Sign Up</NavLink>
+					</div>
 				</>
 			)}
-			</div>
 		</nav>
 	)
 }
